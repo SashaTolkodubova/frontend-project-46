c3bb1af69e86f0dde89baa9c39b812bb92f70b95
SashaTolkodubova <atolkodubova@gmail.com>
2024-11-12 13:33:39 +0300

    step 8

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,17 +9,24 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 // const getFormat = (filename) => path.extname(filename);
 
-const result = readFile('result-stylish.txt');
-// console.log(genDiff(file1, file2));
+const testList = [
+  'yaml',
+  'json',
+];
 
-test('compareFilesJson', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
+const resultStylish = readFile('result-stylish.txt');
+const resultPlain = readFile('result-plain.txt');
+// const file1 = getFixturePath('file1.yaml');
+// const file2 = getFixturePath('file2.yaml');
 
-  expect(genDiff(file1, file2)).toEqual(result);
-});
-test('compareFilesYaml', () => {
-  const file1 = getFixturePath('file1.yaml');
-  const file2 = getFixturePath('file2.yaml');
-  expect(genDiff(file1, file2)).toEqual(result);
+// console.log(genDiff(file1, file2, 'plain'));
+
+describe('gendiff', () => {
+  test.each(testList)('gendiff %s', (format) => {
+    const file1 = getFixturePath(`file1.${format}`);
+    const file2 = getFixturePath(`file2.${format}`);
+    expect(genDiff(file1, file2)).toEqual(resultStylish);
+    expect(genDiff(file1, file2, 'stylish')).toEqual(resultStylish);
+    expect(genDiff(file1, file2, 'plain')).toEqual(resultPlain);
+  });
 });
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,15 +1,16 @@
-// import makePlain from './plain.js';
+import makePlain from './plain.js';
+
 import { makeStylishDiff } from './stylish.js';
 
 export default function formatter(tree, format) {
-  if (format === 'stylish') {
-    return makeStylishDiff(tree);
-    // case 'plain':
-    //   return makePlain(tree);
+  switch (format) {
+    case 'stylish':
+      return makeStylishDiff(tree);
+    case 'plain':
+      return makePlain(tree);
     // case 'json':
     //   return JSON.stringify(tree);
-    // default:
-    //   throw new Error('Uncorrect data');
+    default:
+      throw new Error('Uncorrect data');
   }
-  return makeStylishDiff(tree);
 }
